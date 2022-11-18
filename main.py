53ea739a2f3435f91394b2a53eecfbe7ca8d0b8d
Jomar Ferreira <47539152+joeywheelersam@users.noreply.github.com>
2022-11-18 15:36:12 -0300

    Projeto Final do Desafio 04 da Maratona Python

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests, os
+
+# Função para formatar a lista colocando o https://www quando necessário e deixando em minúsculo
+def formatar_url (lista):
+    for lista in lista.lower().split(","):
+        lista = lista.strip()
+        if "http://www." in lista:
+            url.append(lista)
+        elif "www." in lista:
+            url.append("http://"+lista)
+        else:
+            url.append("http://www."+lista)
+
+# Função para testar as URLs e imprime na tela os resultados
+def testando_url (site):
+    for solicitar in site:
+        if ".com" in solicitar:
+            try:
+                r = requests.get(solicitar)
+                if (r.status_code == 200):
+                    print (f"{solicitar} - Site online. - {r.status_code}")
+                else:
+                    print(f"{solicitar} - Site offline. - {r.status_code}")
+            except:
+                print (f"{solicitar} - URL inválida.")
+        else:
+            print (f"{solicitar} - URL inválida.")
+
+# Repetidor para fechar o programa ou abrir novamente caso o usuário queira
+# A lista sempre vai se repetir de forma vazia
+contador = 0
+while contador == 0:
+    url = []
+    print ("Bem-vindo ao verificador de sites 1.0!")
+    resposta = str (input ("Insira as URLs dos sites que dejesa verificar o status (separe por vírgula): "))
+    print ("\n###############################")
+    formatar_url(resposta)
+    testando_url(url)
+    print ("###############################\n")
+    while contador == 0:
+        resposta = input ("Precisa verificar mais algum site? s/n: ").lower()
+        if resposta == "s":
+            os.system('cls' if os.name == 'nt' else 'clear')
+            break
+        elif resposta == "n":
+            print ("Programa encerrado.")
+            contador = 1
+        else:
+            print ("Valor inválido")
